52b5329cb711bb9075a3ee322c77ebe13bea6d95
Jakub Jozefowicz <kuben.joz@gmail.com>
2024-05-12 22:05:51 +0200

    before change to optim

--- src/main.py
+++ src/main.py
@@ -9,48 +9,33 @@ from readData import readTestFromFile
 @dataclass
 class Player:
     num_resources: int
-    strategy: List[float]
+    strategy: np.ndarray
 
-
+# this is equivalent to finding k'th (k = num_resources) largest element and then all larger so O(n) instead of O(nlog(n))
 def bestPureResponseAtt(att_player, def_player, battlefields):
     num_resources = att_player.num_resources
     def_strat = def_player.strategy
-    for v in def_strat:
+    for v in def_strat: # todo remove
         assert v >= 0.0 and v <= 1.0
-    field_val = []
-    for i, (prob, util) in enumerate(zip(def_strat, battlefields, strict=True)):
-        field_val.append(((1 - prob) * util, i))
-    field_val = sorted(field_val)[
-        -num_resources:
-    ]  # find nth largest then iterate for O(n) or serach kth largest individually
-    res = [0.0] * len(def_strat)
-    for _, i in field_val:
-        res[i] = 1.0  # todo maybe ints or bools
+    utils = (1.0-def_strat) * battlefields
+    util_idxs = np.argpartition(utils, -num_resources) 
+    res = np.zeros_like(def_strat)
+    res[util_idxs[-num_resources:]] = 1.0
     return res
 
-
+# this is equivalent to finding k'th (k = num_resources) largest element and then all larger so O(n) instead of O(nlog(n))
 def bestPureResponseDef(att_player, def_player, battlefields):
     num_resources = def_player.num_resources
     att_strat = att_player.strategy
-    for v in att_strat:
+    for v in att_strat: #todo remove
         assert v >= 0.0 and v <= 1.0
-    field_val = []
-    for i, (prob, util) in enumerate(zip(att_strat, battlefields, strict=True)):
-        field_val.append((prob * util, i))
-    field_val = sorted(field_val)[
-        -num_resources:
-    ]  # find nth largest then iterate for O(n) or serach kth largest individually
-    res = [0.0] * len(att_strat)
-    for _, i in field_val:
-        res[i] = 1.0  # todo maybe ints or bools
+    utils = att_strat * battlefields
+    util_idxs = np.argpartition(utils, -num_resources)
+    res = np.zeros_like(att_strat)
+    res[util_idxs[-num_resources:]] = 1.0
     return res
-1000
-(1,2)
-(1,900)
-(500,900)
-(899,900)
-for i in range(900):
-    for j in range(0,900):
+
+
 def getEpsilon(
     att_player, def_player, battlefields, best_resp_att=None, best_resp_def=None
 ):
@@ -60,11 +45,8 @@ def getEpsilon(
         best_resp_att = bestPureResponseAtt(att_player, def_player, battlefields)
     if best_resp_def is None:
         best_resp_def = bestPureResponseDef(att_player, def_player, battlefields)
-    pay_att = 0.0
-    pay_def = 0.0
-    for i in range(n):
-        pay_att += best_resp_att[i] * battlefields[i] * (1 - def_strat[i])
-        pay_def += (1 - best_resp_def[i]) * battlefields[i] * att_strat[i]
+    pay_att = np.sum(best_resp_att * battlefields * (1 - def_strat))
+    pay_def = np.sum((1 - best_resp_def) * battlefields * att_strat)
 
     return abs(pay_att - pay_def)
 
@@ -75,9 +57,10 @@ def ficticiousPlay(  # todo intial conditions player
     assert num_res_att > 0 and num_res_att < len(battlefields)
     assert num_res_def > 0 and num_res_def < len(battlefields)
     assert num_res_att < num_res_def
-    num_battlefields = len(battlefields)
-    att_play = Player(num_res_att, [0.0] * num_battlefields)
-    def_play = Player(num_res_def, [0.0] * num_battlefields)
+    #battlefields = np.array(battlefields, dtype=np.double)
+    num_battlefields = battlefields.shape[0]
+    att_play = Player(num_res_att, np.array([num_res_att/num_battlefields] * num_battlefields, dtype=np.double)) # good default init
+    def_play = Player(num_res_def, np.array([num_res_def/num_battlefields] * num_battlefields, dtype=np.double)) # good default init
     epsilons = []
     for t in range(1, max_iters + 1):
         resp_att = bestPureResponseAtt(att_play, def_play, battlefields)
@@ -91,7 +74,7 @@ def ficticiousPlay(  # todo intial conditions player
         for i, (cur, new) in enumerate(zip(def_play.strategy, resp_def)):
             def_play.strategy[i] = (cur * (t - 1) + new) / t
 
-    return att_play.strategy, def_play.strategy, epsilons
+    return att_play.strategy, def_play.strategy, np.array(epsilons) # len(epsilons) gives us the number of iterations the algorithm ran for
 
 
 def getPayouts(stratA, stratD):
@@ -121,15 +104,16 @@ def getPayouts(stratA, stratD):
     return res1, res2
 
 
-n, bA, bD, bfs = readTestFromFile(sys.argv[1])
-# n, bA, bD, bfs = readTestFromFile("test.test")
+#n, bA, bD, bfs = readTestFromFile(sys.argv[1])
+n, bA, bD, bfs = readTestFromFile("../tests/high50.in")
 
 
 print(n, bA, bD, bfs)
 
-att_strat, def_strat, epsilons = ficticiousPlay(bfs, bA, bD, max_iters=10000)
+att_strat, def_strat, epsilons = ficticiousPlay(np.array(bfs), bA, bD, max_iters=10_000)
 num_iters = len(epsilons)
 
 print(att_strat)
 print(def_strat)
 print(len(epsilons))
+print(epsilons[-1])
