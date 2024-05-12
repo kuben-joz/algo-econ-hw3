a064ddd76fa7dbac3042373cc12acc5e86bf4ec2
Jakub Jozefowicz <kuben.joz@gmail.com>
2024-05-12 13:00:10 +0200

    best pure response unoptimal

--- src/main.py
+++ src/main.py
@@ -1,6 +1,47 @@
 import sys
+from dataclasses import dataclass
+from typing import List
+import numpy as np
 
 from readData import readTestFromFile
+
+
+@dataclass
+class Player:
+    num_resources: int
+    strategy: List[float]
+
+
+@dataclass
+class Battlefield:
+    num_arenas: int
+    arena_utils: List[int]
+
+
+def bestPureResponseAtt(num_resources, def_strat, battlefield_utils):
+    for v in def_strat:
+        assert(v >= 0.0 and v <= 1.0)
+    field_val = []
+    for i, prob, util in enumerate(zip(def_strat, battlefield_utils)):
+        field_val[i] = ((1-prob) * util, i)
+    field_val = sorted(field_val)[-num_resources:] # find nth largest then iterate for O(n) or serach kth largest individually
+    res = [0]*len(def_strat)
+    for _, i in field_val:
+        res[i] = 1.0 # todo maybe ints or bools
+    return res
+
+
+def bestPureResponseDef(num_resources, att_strat, battlefield_utils):
+    for v in att_strat:
+        assert(v >= 0.0 and v <= 1.0)
+    field_val = []
+    for i, prob, util in enumerate(zip(att_strat, battlefield_utils)):
+        field_val[i] = (prob * util, i)
+    field_val = sorted(field_val)[-num_resources:] # find nth largest then iterate for O(n) or serach kth largest individually
+    res = [0]*len(att_strat)
+    for _, i in field_val:
+        res[i] = 1.0 # todo maybe ints or bools
+    return res
 
 def getPayouts(stratA, stratD):
     if len(stratA) < n or len(stratD) < n:
@@ -10,7 +51,9 @@ def getPayouts(stratA, stratD):
 
     for i in range(n):
         if (stratA[i] != 0 and stratA[i] != 1) or (stratD[i] != 0 and stratD[i] != 1):
-            raise Exception("Players' strategies have to be 0 or 1 for each battlefield")
+            raise Exception(
+                "Players' strategies have to be 0 or 1 for each battlefield"
+            )
 
         used1 += stratA[i]
         used2 += stratD[i]
@@ -20,11 +63,13 @@ def getPayouts(stratA, stratD):
             res2 -= bfs[i]
 
     if used1 != bA or used2 != bD:
-        raise Exception("Players' strategies have to add up to number of their resources")
-        
+        raise Exception(
+            "Players' strategies have to add up to number of their resources"
+        )
+
     return res1, res2
 
 
 n, bA, bD, bfs = readTestFromFile(sys.argv[1])
 
-print (n, bA, bD, bfs)
+print(n, bA, bD, bfs)
